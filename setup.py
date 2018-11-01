27f9594dad7dcac7df5dda98c796b86657037d0f
Valentin Louf <v.louf@bom.gov.au>
2018-11-01 11:43:47 +1100

    UTF8 encoding in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # Learn more: https://github.com/kennethreitz/setup.py
-
+import io
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with io.open('README.md', "r", encoding="utf-8") as f:
     readme = f.read()
 
 with open('LICENSE') as f:
