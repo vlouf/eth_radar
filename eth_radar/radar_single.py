d27c68aa980611c921bd23cb71c61f31c56ec7c9
vlouf <v.louf@bom.gov.au>
2019-03-20 13:32:06 +1100

    TODO: saving data. Processing done.

--- eth_radar/radar_single.py
+++ eth_radar/radar_single.py
@@ -18,13 +18,16 @@ into what is expected for the echotop.py script to run. It also saves the data.
 import os
 import argparse
 import warnings
+import traceback
 
 # Other Libraries
+import xarray as xr
+import numpy as np
 import crayons
 import echotop
 
 
-def save_data():
+def save_data(radar, cth_grid):
     """
     TODO: save output data function.
     """
@@ -34,8 +37,52 @@ def save_data():
 
 def main():
     """
-    TODO: Read data, format them into the appropriate args needed to call echotop.py.
+    Read input radar file, extract the reflectivity, compute the echo top height
+    (0 dB by default) and save the result.
+
+    Arguments:
+    ==========
+    INFILE: global str
+        Input file name
+    ETH_THLD: global float
+        Echo-top-height threshold (default 0 dB).
+    REFL_NAME: global str
+        Name of the reflectivity field used to compute the echo top height
+        ('reflectivity' by default) in the radar data.
     """
+    # Load data
+    radar = xr.open_dataset(INFILE)
+    r = radar.range.values
+    azimuth = radar.azimuth.values
+    elevation = radar.elevation.values
+    try:
+        refl = radar[REFL_NAME].values
+    except KeyError:
+        traceback.print_exc()
+        print(crayons.red(f'Error: wrong reflectivity field name. You gave {REFL_NAME}. ' +
+                          f'The possible field names are: {radar.keys()}'))
+
+    st_sweep = radar.sweep_start_ray_index.values
+    ed_sweep = radar.sweep_end_ray_index.values
+    print(crayons.green(f'{os.path.basename(INFILE)} data loaded.'))
+
+    # Compute ETH
+    cth = echotop.compute_cloud_top(r, azimuth, elevation, st_sweep, ed_sweep, refl, eth_thld=ETH_THLD)
+    print(crayons.green(f'{ETH_THLD}-dB echo top height computed on polar coordinates.'))
+
+    # Grid data
+    th = 450 - azimuth[slice(st_sweep[0], ed_sweep[0] + 1)]
+    th[th < 0] += 360
+
+    R, A = np.meshgrid(r, th)
+    x = R * np.cos(np.pi * A / 180)
+    y = R * np.sin(np.pi * A / 180)
+
+    xgrid = np.arange(-145e3, 146e3, 2500)
+    cth_grid = echotop.grid_radar(cth, x, y, xgrid, xgrid)
+    print(crayons.green(f'Data gridded.'))
+
+    save_data(radar, cth_grid)
 
     return None
 
@@ -70,10 +117,19 @@ calculation, and rainfall rate estimation."""
         type=float,
         help='Echo top height threshold (e.g. 0, 5, 17, ...). 0 dB by default.',
         default=0.0)
+    parser.add_argument(
+        '-f',
+        '--dbz-name',
+        dest='db_name',
+        type=str,
+        help='Radar reflectivity field name.',
+        default='reflectivity')
+
 
     args = parser.parse_args()
     INFILE = args.infile
     OUTPATH = args.outdir
     ETH_THLD = args.eth_thld
+    REFL_NAME = args.db_name
 
     main()
