0f889de08000f97f0689af236baaafc24e86c8c4
vlouf <v.louf@bom.gov.au>
2019-03-20 13:58:19 +1100

    Updated setup.py to install required packages.

--- setup.py
+++ setup.py
@@ -11,14 +11,32 @@ with io.open('README.md', "r", encoding="utf-8") as f:
 with open('LICENSE') as f:
     license = f.read()
 
+# What packages are required for this module to be executed?
+REQUIRED = [
+    "numpy", "crayons", "netCDF4", "xarray", "numba"
+]
+
 setup(
     name='eth_radar',
-    version='0.1.0',
+    version='1.0',
     description='Echo top height estimation from radar data.',
     long_description=readme,
     author='Valentin Louf',
-    author_email='valentin.louf@bom.gov.au',
+    author_email='valentin.louf@monash.edu',
     url='https://github.com/vlouf/eth_radar',
     license=license,
-    packages=find_packages(exclude=('notebook'))
+    install_requires=REQUIRED,
+    include_package_data=True,
+    packages=find_packages(exclude=('notebook')),
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Intended Audience :: Science/Research',
+    ],
 )
